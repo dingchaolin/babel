431b9682b8fad6fd0ea1142a128470475a28171c
dingchaolin <18810042351@163.com>
2016-10-08 23:24:13 +0800

    babel2324save

---
+++ src/jsx.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2016/10/8 0008.
+ */
+import React,{Component} from 'react';
+
+class DemoComponent extends Component{
+    render(){
+        return <h1>Hello World!</h1>
+    }
+}
